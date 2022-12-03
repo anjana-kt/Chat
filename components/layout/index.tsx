8f648c3fd0b595f88d35a1b79f3c3a5e325738de
ByteCrak07 <abilsavio@gmail.com>
2022-12-04 00:16:43 +0530

    Add connect wallet

---
+++ components/layout/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode, FC } from "react";
+import Footer from "./footer";
+import Header from "./header";
+import Toast from "./toast";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="bg-slate-900 min-h-screen text-white flex flex-col">
+      <Header />
+
+      <main className="flex-grow flex items-center justify-center">
+        {children}
+      </main>
+
+      <Toast />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout";
+import WalletAuthWrapper from "../contexts/WalletAuthWrapper";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletAuthWrapper>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </WalletAuthWrapper>
+  );
 }
