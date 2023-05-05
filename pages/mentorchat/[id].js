971071f6a7ed00dc3d38a85517ffd872cecfb956
rishika <rishikarocky14@gmail.com>
2023-05-05 16:33:57 +0530

    supporter page done

---
+++ pages/mentorchat/[id].js
@@ -0,0 +1,44 @@
+import styles from "../../styles/Home.module.css";
+import { Chat } from "@pushprotocol/uiweb";
+import { useRouter } from "next/router";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import baseUrl from "../../components/baseUrl";
+
+export default function Support() {
+//   const router = useRouter();
+//   const { id } = router.query;
+  const [supporter, setSupporter] = useState(null);
+  const [request, setRequest] = useState([]);
+
+  // useEffect(() => {
+  //   Axios.get(`${baseUrl}/api/get_request?id=${id}`)
+  //     .then((res) => {
+  //       setRequest(res.data);
+  //       // console.log();
+  //     })
+
+  //     setTimeout(10000, () => {
+  //       console.log("Hello")
+  //       setSupporter(res.data[0]["supporter_address"]);
+  //       console.log(supporter)
+
+  //     });
+  // }, []);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Chat
+            supportAddress="0x4be5ed1f1087015a10a37bed15fe70b7c23e182e" //user address
+            account="0x0C28f1edd0784B58E8eB525149E80A4353022968" //support address
+            apiKey="xkxLiG74pc.A3n2bD4wWFNafRerPJSx1qj2KMRnmhuoFgsxCJIOzRezxmCgmv5Xc7bqhKLRCTVQ"
+            modalTitle="Help line"
+            env="staging"
+          />
+        </main>
+      </div>
+    </>
+  );
+}
