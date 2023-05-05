f59d90faa5aa347bc59eae53753261e0b9a718a0
Prometheus2k <shonesk2000@gmail.com>
2023-05-05 09:13:49 +0530

    Added organization functions

--- backend/contracts/ChainAid.sol
+++ backend/contracts/ChainAid.sol
@@ -9,8 +9,22 @@ contract ChainAid {
         owner = payable(msg.sender);
     }
 
+    function isDeployer(address _address) public view returns (bool) {
+        if (_address == owner) return true;
+        else return false;
+    }
+
+    function changeDeployer(address _address) public {
+        require(msg.sender == owner, "you are not the deployer");
+        owner = payable(_address);
+    }
+
     struct Org {
+        uint256 id;
         string name;
+        string email;
+        string contactNo;
+        address _orgAddress;
         uint256 balance;
         address[] supporters;
     }
@@ -20,20 +34,64 @@ contract ChainAid {
         address self;
     }
 
-    mapping(address => Org) Organization;
+    uint public orgCount;
+
+    mapping(address => Org) public Organization;
+    mapping(address => bool) RegisteredOrganization;
     address[] allOrganizations;
 
     mapping(address => Member) Members;
     address[] allMembers;
 
-    function addOrg(string calldata name) public payable {
+    function addOrg(
+        string memory name,
+        string memory email,
+        string memory contactNo
+    ) public payable {
         require(msg.value >= 1e17, "Error, deposit must be >= 10 MATIC");
-
-        Organization[msg.sender].name = name;
-        Organization[msg.sender].balance = msg.value;
-
+        RegisteredOrganization[msg.sender] = true;
+        orgCount++;
+        Organization[msg.sender] = Org(
+            orgCount,
+            name,
+            email,
+            contactNo,
+            msg.sender,
+            msg.value,
+            new address[](0)
+        );
         allOrganizations.push(msg.sender);
         console.log("Added org");
+    }
+
+    function getAllOrgs() public view returns (Org[] memory) {
+        require(owner == msg.sender);
+        Org[] memory _allOrgs = new Org[](allOrganizations.length);
+
+        for (uint i = 0; i < allOrganizations.length; i++) {
+            _allOrgs[i] = Organization[allOrganizations[i]];
+        }
+        return _allOrgs;
+    }
+
+    function removeOrg(address _addr) public {
+        require(msg.sender == owner, "you are not the deployer");
+        require(RegisteredOrganization[_addr], "No org found");
+        RegisteredOrganization[_addr] = false;
+
+        uint len = allOrganizations.length;
+        for (uint i = 0; i < len; i++) {
+            if (allOrganizations[i] == _addr) {
+                allOrganizations[i] = allOrganizations[len - 1];
+                allOrganizations.pop();
+                break;
+            }
+        }
+    }
+
+    function isOrganization(address _addr) public view returns (bool) {
+        if (RegisteredOrganization[_addr]) return true;
+        else return false;
     }
 
     function addMember(address newOrg) public {
@@ -52,15 +110,5 @@ contract ChainAid {
         uint remainingReward = totalReward - commission;
         owner.transfer(commission);
         member.transfer(remainingReward);
-    }
-
-    function getAllOrgs() public view returns (Org[] memory) {
-        Org[] memory _allOrgs = new Org[](allOrganizations.length);
-
-        for (uint i = 0; i < allOrganizations.length; i++) {
-            _allOrgs[i] = Organization[allOrganizations[i]];
-        }
-
-        return _allOrgs;
     }
 }
